c223df0402ab6380b7cd5185a820417ebe951ac0
Waza <wzaher@tfconcepts.com>
2020-08-26 19:40:34 -0400

    added support to all number, text and boolean fields for products.firestore.set()

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,34 +17,37 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    {
-      resolve: "gatsby-source-firestore",
-      options: {
-        credential: require("./serviceAccountKey.json"),
+    //This is the gatsby way of connecting to Firestore. 
+    // Since I'm the one creating and managing the db and data, I decided not to use graphql until it has some added value. 
+    // so I connect to firestore through its npm package directly.
+    // {
+    //   resolve: "gatsby-source-firestore",
+    //   options: {
+    //     credential: require("./serviceAccountKey.json"),
 
-        types: [
-          {
-            type: "Product",
-            collection: "products",
-            map: doc => ({
-              name: doc.name,
-              dateCreated: doc.dateCreate,
-              id: doc.id,
-              media: doc.media,
-              price: doc.price,
-              stock: doc.stock,
-              notes: doc.notes,
-              description: doc.description,
-              display: doc.display,
-              instructions: doc.instructions,
-              tags: doc.tags
-            }),
-          },
-          {},
-          {},
-        ],
-      },
-    },
+    //     types: [
+    //       {
+    //         type: "Product",
+    //         collection: "products",
+    //         map: doc => ({
+    //           name: doc.name,
+    //           dateCreated: doc.dateCreate.toDate(),
+    //           id: doc.id,
+    //           media: doc.media,
+    //           price: doc.price,
+    //           stock: doc.stock,
+    //           notes: doc.notes,
+    //           description: doc.description,
+    //           display: doc.display,
+    //           instructions: doc.instructions,
+    //           tags: doc.tags,
+    //         }),
+    //       },
+    //       {},
+    //       {},
+    //     ],
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { ProductInput } from "../components/productInput"
 import firebase from "../db/firebase"
+import ListProducts from "./listProducts"
 
 export default function AddProduct() {
   const [products, setProducts] = React.useState([])
@@ -12,34 +13,134 @@ export default function AddProduct() {
   const [newProductNotes, setNewPoductNotes] = React.useState()
   const [newProductDescription, setNewProductDescription] = React.useState()
   const [newProductDisplay, setNewProductDisplay] = React.useState()
-  const [newProductInstructions, setNewProductinstructions] = React.useState()
+  const [newProductInstructions, setNewProductInstructions] = React.useState()
   const [newProductTags, setNewProductTags] = React.useState()
+  const [newProductSalePrice, setNewProductSalePrice] = React.useState()
 
-  React.useEffect(() => {
-    const db = firebase.firestore()
+  //moved to a seperate component ListProducts
+  // React.useEffect(() => {
+  //   const db = firebase.firestore()
 
-    // Here is how you subscribe to the data to update any changes in values in realtime.
-    const unsubscribe = db.collection("products").onSnapshot(snapshot => {
-      const productsData = []
-      snapshot.forEach(doc => productsData.push({ ...doc.data(), id: doc.id }))
-      setProducts(productsData)
-    })
-    return unsubscribe
-  }, [])
+  //   // Here is how you subscribe to the data to update any changes in values in realtime.
+  //   const unsubscribe = db.collection("products").onSnapshot(snapshot => {
+  //     const productsData = []
+  //     snapshot.forEach(doc => productsData.push({ ...doc.data(), id: doc.id }))
+  //     setProducts(productsData)
+  //   })
+  //   return unsubscribe
+  // }, [])
 
   const onAdd = () => {
     const db = firebase.firestore()
-    db.collection("products").add({ name: newProductName })
+
+    // const newProductMediaArray = JSON.stringify(newProductMedia).split(",")
+
+    const newProduct = {
+      name: newProductName,
+      // media: [newProductMediaArray],
+      price: Number(newProductPrice),
+      salePrice: Number(newProductSalePrice),
+      stock: Number(newProductStock),
+      notes: newProductNotes,
+      description: newProductDescription,
+      display: (document.getElementById("newProductDisplay").checked == true),
+      instructions: newProductInstructions,
+      // tags: newProductTags,
+    }
+    db.collection("products").add(newProduct)
   }
 
   return (
     <Layout>
       <ul>
+        <label for="newProductName">Name </label>
         <input
+          id="newProductName"
           value={newProductName}
           onChange={e => setNewProductName(e.target.value)}
         />
-        <button onClick={onAdd}>Add</button>
+        <br />
+
+        <label for="newProductMedia">Media </label>
+        <input
+          id="newProductMedia"
+          type="text"
+          value={newProductMedia}
+          onChange={e => setNewProductMedia(e.target.value.split(", "))}
+        />
+        <br />
+
+        <label for="newProductPrice">Price </label>
+        <input
+          type="number"
+          id="newProductPrice"
+          value={newProductPrice}
+          onChange={e => setNewProductPrice(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductStock">Stock </label>
+        <input
+          id="newProductStock"
+          type="number"
+          value={newProductStock}
+          onChange={e => setNewProductStock(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductSalePrice">Sale Price </label>
+        <input
+          id="newProductSalePrice"
+          type="number"
+          value={newProductSalePrice}
+          onChange={e => setNewProductSalePrice(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductNotes">Notes </label>
+        <input
+          id="newProductNotes"
+          value={newProductNotes}
+          onChange={e => setNewPoductNotes(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductDescription">Description </label>
+        <input
+          id="newProductDescription"
+          value={newProductDescription}
+          onChange={e => setNewProductDescription(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductInstructions">Instructions </label>
+        <input
+          id="newProductInstructions"
+          value={newProductInstructions}
+          onChange={e => setNewProductInstructions(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductTags">Tags </label>
+        <input
+          id="newProductTags"
+          value={newProductTags}
+          onChange={e => setNewProductTags(e.target.value)}
+        />
+        <br />
+
+        <label for="newProductDisplay">Show product? </label>
+        <input
+          type="checkbox"
+          id="newProductDisplay"
+          name="on"
+          // value={newProductDisplay}
+          checked
+          onChange={e => setNewProductDisplay(e.target.value)}
+        />
+        <br />
+
+        <button onClick={onAdd}>Add New Product</button>
 
         {products.map(product => (
           <li key={product.name}>
@@ -47,6 +148,7 @@ export default function AddProduct() {
           </li>
         ))}
       </ul>
+      {/* <ListProducts /> */}
     </Layout>
   )
 }
